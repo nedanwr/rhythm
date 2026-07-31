fa0076b86adedc317bf5bf88f8d4f6cf30cc32da
Naveed <46922100+nedanwr@users.noreply.github.com>
2026-08-01 04:08:19 +0800

    fix(web): update import path for router to use alias for improved module resolution

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./routes/router";
+
+import { router } from "~/routes/router";
 import "./styles.css";
 
 const queryClient = new QueryClient({
